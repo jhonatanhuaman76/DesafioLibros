cbb3b78151a7fd987a50666a6d4199d5fe9fe5d5
Jhonatan <jhonatanhuaman76@gmail.com>
2024-07-07 12:32:43 -0500

    Primer commit

---
+++ src/main/java/com/aluracursos/DesafioLibros/DesafioLibrosApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.DesafioLibros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioLibrosApplication.class, args);
+	}
+
+}
